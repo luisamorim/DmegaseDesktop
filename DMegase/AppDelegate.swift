91920c0dfece7d81ec43457b693577d00840e365
Luis Henrique de Oliveira Amorim <lamorim@scopus.com.br>
2018-05-05 10:47:29 -0300

    Initial Commit

---
+++ DMegase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DMegase
+//
+//  Created by Luis Henrique de Oliveira Amorim on 5/5/18.
+//  Copyright © 2018 Luis Henrique de Oliveira Amorim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DMegase/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DMegase
+//
+//  Created by Luis Henrique de Oliveira Amorim on 5/5/18.
+//  Copyright © 2018 Luis Henrique de Oliveira Amorim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
